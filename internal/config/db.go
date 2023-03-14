cc6c550f68c432c3391b765ac323cdccbcb096bf
Mokhlesur Rahman <mokhlesurr031@gmail.com>
2023-03-14 06:59:23 +0600

    feat(ci): github ci

--- internal/config/db.go
+++ internal/config/db.go
@@ -31,25 +31,13 @@ func loadDB() {
 	}
 
 	currentDB := viper.GetString("current_db.RUNNING")
-	if currentDB == "docker_database" {
-		db = Database{
-			Name:     viper.GetString(currentDB + ".POSTGRESQL_ADDON_DB"),
-			Username: viper.GetString(currentDB + ".POSTGRESQL_ADDON_USER"),
-			Password: viper.GetString(currentDB + ".POSTGRESQL_ADDON_PASSWORD"),
-			Host:     viper.GetString(currentDB + ".POSTGRESQL_ADDON_HOST"),
-			Port:     viper.GetInt(currentDB + ".POSTGRESQL_ADDON_PORT"),
-		}
-
-	} else if currentDB == "remote_db" {
-		viper.AutomaticEnv()
-		db = Database{
-			Name:     viper.GetString("POSTGRESQL_ADDON_DB"),
-			Username: viper.GetString("POSTGRESQL_ADDON_USER"),
-			Password: viper.GetString("POSTGRESQL_ADDON_PASSWORD"),
-			Host:     viper.GetString("POSTGRESQL_ADDON_HOST"),
-			Port:     viper.GetInt("POSTGRESQL_ADDON_PORT"),
-		}
-		fmt.Println("DATABASEEEE", db)
-
+	db = Database{
+		Name:     viper.GetString(currentDB + ".POSTGRESQL_ADDON_DB"),
+		Username: viper.GetString(currentDB + ".POSTGRESQL_ADDON_USER"),
+		Password: viper.GetString(currentDB + ".POSTGRESQL_ADDON_PASSWORD"),
+		Host:     viper.GetString(currentDB + ".POSTGRESQL_ADDON_HOST"),
+		Port:     viper.GetInt(currentDB + ".POSTGRESQL_ADDON_PORT"),
 	}
+	fmt.Println("DBBB", db)
+
 }
